761ba297c12b9504cebb29111bec7b073ce55151
iamsajana <sajana.bhaskaran@gmail.com>
2021-03-01 21:11:06 +0100

    removed unused import files

--- src/main/java/com/toDoList/ShowTask.java
+++ src/main/java/com/toDoList/ShowTask.java
@@ -1,17 +1,12 @@
 package com.toDoList;
 
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicReference;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class ShowTask {
-
     public void filterOption(Path filePath) {
         Scanner scannerinput = new Scanner(System.in);
         System.out.println("ENTER THE OPTION 1 TO SORT BY DATE:  ");
@@ -37,27 +32,25 @@ public class ShowTask {
     private void sortByDate(Path filePath) {
         TaskFileHandler taskFileHandler = new TaskFileHandler();
         List<Task> taskList = taskFileHandler.convertFilesToList(filePath);
-        Collections.sort(taskList,((o1, o2) -> {
-            Task task1= (Task) o1;
-            Task task2= (Task) o2;
+        Collections.sort(taskList, ((o1, o2) -> {
+            Task task1 = (Task) o1;
+            Task task2 = (Task) o2;
             return task1.getDueDate().compareTo(task2.getDueDate());
         }));
         System.out.println("TASCS SORTED BY DUE DATE ORDER: ");
         taskList.stream().forEach(System.out::println);
-
     }
 
     private void sortByProject(Path filePath) {
         TaskFileHandler taskFileHandler = new TaskFileHandler();
         List<Task> taskList = taskFileHandler.convertFilesToList(filePath);
-        Collections.sort(taskList,((o1, o2) -> {
-            Task task1= (Task) o1;
-            Task task2= (Task) o2;
+        Collections.sort(taskList, ((o1, o2) -> {
+            Task task1 = (Task) o1;
+            Task task2 = (Task) o2;
             return task1.getProject().toUpperCase().compareTo(task2.getProject().toUpperCase());
         }));
         System.out.println("TASCS SORTED BY PROJECT'S ALPHABETICAL ORDER: ");
         taskList.stream().forEach(System.out::println);
-
     }
 
     public List<Task> showAllTasks(Path filePath) {
