7e5cee5e7f55f3cd4ac03f2480e0da3c02fe0fb9
iamsajana <sajana.bhaskaran@gmail.com>
2021-03-01 20:50:49 +0100

    updated and modified the show task code

--- src/main/java/com/toDoList/ShowTask.java
+++ src/main/java/com/toDoList/ShowTask.java
@@ -14,9 +14,9 @@ public class ShowTask {
 
     public void filterOption(Path filePath) {
         Scanner scannerinput = new Scanner(System.in);
-        System.out.println("Enter 1 if you want to sort by date ");
-        System.out.println("Enter 2 if you want to sort by Project");
-        System.out.println("Enter 3 to Quit");
+        System.out.println("ENTER THE OPTION 1 TO SORT BY DATE:  ");
+        System.out.println("ENTER THE OPTION 2 TO SORT BY PROJECT: ");
+        System.out.println("ENTER THE OPTION 3 TO QUIT: ");
         String option = scannerinput.nextLine();
         switch (option) {
             case "1":
@@ -37,12 +37,21 @@ public class ShowTask {
                 System.exit(1);
                 break;
             default:
-                System.out.println("Invalid option, Please enter again!!");
+                System.out.println("INVALID OPTION, PLEASE ENTER AGAIN!!");
                 filterOption(filePath);
         }
     }
 
     private void sortByDate(Path filePath) {
+        TaskFileHandler taskFileHandler = new TaskFileHandler();
+        List<Task> taskList = taskFileHandler.convertFilesToList(filePath);
+        Collections.sort(taskList,((o1, o2) -> {
+            Task task1= (Task) o1;
+            Task task2= (Task) o2;
+            return task1.getDueDate().compareTo(task2.getDueDate());
+        }));
+        System.out.println("TASCS SORTED BY DUE DATE ORDER: ");
+        taskList.stream().forEach(System.out::println);
 
     }
 
@@ -54,7 +63,7 @@ public class ShowTask {
             Task task2= (Task) o2;
             return task1.getProject().toUpperCase().compareTo(task2.getProject().toUpperCase());
         }));
-        System.out.println("All the tasks sorted by Project: ");
+        System.out.println("TASCS SORTED BY PROJECT'S ALPHABETICAL ORDER: ");
         taskList.stream().forEach(System.out::println);
 
     }
