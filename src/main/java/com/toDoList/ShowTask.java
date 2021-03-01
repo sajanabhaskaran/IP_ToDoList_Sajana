00c93199a5bcd27c34993264a1b0e8db0135c2e8
iamsajana <sajana.bhaskaran@gmail.com>
2021-03-01 20:55:16 +0100

    removed code for filtering

--- src/main/java/com/toDoList/ShowTask.java
+++ src/main/java/com/toDoList/ShowTask.java
@@ -20,17 +20,9 @@ public class ShowTask {
         String option = scannerinput.nextLine();
         switch (option) {
             case "1":
-                /*Scanner inputDate = new Scanner(System.in);
-                System.out.println("Enter the Date (dd/mm/yyyy): ");
-                String filterDate = inputDate.nextLine();
-                showTaskFileByDate(filePath, filterDate);*/
                 sortByDate(filePath);
                 break;
             case "2":
-                /*Scanner inputProject = new Scanner(System.in);
-                System.out.println("Enter the Project: ");
-                String filterProject = inputProject.nextLine();
-                showTaskFileByProject(filePath, filterProject);*/
                 sortByProject(filePath);
                 break;
             case "3":
@@ -67,51 +59,6 @@ public class ShowTask {
         taskList.stream().forEach(System.out::println);
 
     }
-
-    /*private void showTaskFileByProject(Path filePath, String filterProject) {
-
-        TaskFileHandler taskFileHandler = new TaskFileHandler();
-        List<Task> taskList = taskFileHandler.convertFilesToList(filePath);
-
-        List<Task> filterTask = taskList.stream().filter(task -> {
-            if (task.getProject().equals(filterProject)) return true;
-            return false;
-        }).collect(Collectors.toList());
-
-        if (!filterTask.isEmpty()){
-            filterTask.stream().forEach(System.out::println);
-        }
-        else {
-            System.out.println("There is no tasks with the specified Project!!");
-        }
-
-
-
-    }*/
-
-    /*public void showTaskFileByDate(Path filePath, String date) {
-
-
-        TaskFileHandler taskFileHandler = new TaskFileHandler();
-        List<Task> taskList = taskFileHandler.convertFilesToList(filePath);
-
-
-        List<Task> filterTask = taskList.stream().filter(task -> {
-            if (task.getDueDate().equals(date)) return true;
-            return false;
-        }).collect(Collectors.toList());
-
-        if (!filterTask.isEmpty()) {
-            System.out.println("\n");
-            System.out.println("THESE ARE THE TASKS ON THE DATE :" + date);
-            System.out.println("\n");
-            filterTask.stream().forEach(System.out::println);
-        } else {
-            System.out.println("There is no tasks in the specified date!!");
-        }
-
-
-    }*/
 
     public List<Task> showAllTasks(Path filePath) {
         AtomicReference<Integer> n = new AtomicReference<>(1);
